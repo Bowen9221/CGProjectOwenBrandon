c781ef34e471bce91358260b6bfb872174212aeb
CameronJackman <cameron.jackman@ontariotechu.net>
2025-09-18 15:47:32 -0400

    Unity Setup

---
+++ Assets/Scripts/Used Tutorials.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsedTutorials : MonoBehaviour
+{
+    //Movement https://www.youtube.com/watch?v=WIl6ysorTE0 
+
+    //Understanding of Cinemachine https://www.youtube.com/watch?v=asruvbmUyw8
+
+}
