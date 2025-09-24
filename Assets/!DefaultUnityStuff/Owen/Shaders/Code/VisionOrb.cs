c04d04514e72987700d44b50459bbc1de6dc9d3d
Bowen9221 <owen.brandon@ontariotechu.net>
2025-09-23 23:44:34 -0400

    VISION LIMITER BABEEEEE
    Finally I'm done with this stupid shader I never want to touch it again because of how damn annoying it was
    
    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Assets/!DefaultUnityStuff/Owen/Shaders/Code/VisionOrb.cs
+++ Assets/!DefaultUnityStuff/Owen/Shaders/Code/VisionOrb.cs
@@ -1,8 +1,35 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VisionOrb : MonoBehaviour
 {
+    public Material visionLimiter;
+    public Transform fox;
+    public float visionRadius;
+    public float feather;
+    
 
+    public CinemachineBrain brain;
+    public CinemachineFreeLook foxVCam;
+    public CinemachineFreeLook humanVCam;
+
+    private void Update()
+    {
+        visionLimiter.SetVector("_FoxPos", fox.position);
+        visionLimiter.SetFloat("_VisionRadius", visionRadius);
+        visionLimiter.SetFloat("Feather", feather);
+
+        ICinemachineCamera activeCam = brain.ActiveVirtualCamera;
+        if (activeCam == foxVCam)
+        {
+            visionLimiter.SetFloat("_IsFox", 1f);
+        }
+        else if (activeCam == humanVCam)
+        {
+            visionLimiter.SetFloat("_IsFox", 0f);
+        }
+        Debug.Log(visionLimiter.GetFloat("_IsFox"));
+    }
 }
